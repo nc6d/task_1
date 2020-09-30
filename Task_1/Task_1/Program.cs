652ffe252cfab3ee154e950f2f836894de59669b
nc6d <6od92002@gmail.com>
2020-09-30 12:57:54 +0300

    input encoding

--- Task_1/Task_1/Program.cs
+++ Task_1/Task_1/Program.cs
@@ -7,6 +7,7 @@ namespace Task_1
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
 
             Console.WriteLine("Привіт, ДонНУ!");
         }
